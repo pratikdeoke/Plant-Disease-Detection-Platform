e8bb0202c70cb98264c3119f998855771091affd
pratikdeoke <deokepratik3@gmail.com>
2026-01-23 14:55:47 +0530

    discussion routes modified

--- backend/src/routes/discussion.routes.js
+++ backend/src/routes/discussion.routes.js
@@ -1,25 +1,3 @@
-// import { Router } from "express";
-// import {
-//   createPost,
-//   getAllPosts,
-//   getPostWithComments,
-//   addComment,
-//   deletePost
-// } from "../controllers/discussion.controller.js";
-
-// import { authenticate } from "../middleware/auth.middleware.js";
-// import { authorize } from "../middleware/rbac.middleware.js";
-
-// const router = Router();
-
-// router.post("/", authenticate, createPost);
-// router.get("/", getAllPosts);
-// router.get("/:id", getPostWithComments);
-// router.post("/:id/comments", authenticate, addComment);
-// router.delete("/:id", authenticate, authorize(["ADMIN"]), deletePost);
-
-// export default router;
-
 import express from "express";
 import {
   createPost,
