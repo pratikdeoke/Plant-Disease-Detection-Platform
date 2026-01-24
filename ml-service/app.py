d863fd785882f3ff76ae5cd2baead955ec2bf698
pratikdeoke <deokepratik3@gmail.com>
2026-01-24 11:14:53 +0530

    files added

--- ml-service/app.py
+++ ml-service/app.py
@@ -20,5 +20,8 @@ def predict():
     result = predict_image(image)
     return jsonify(result)
 
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
