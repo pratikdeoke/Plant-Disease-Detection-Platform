13f59fe39c18068dbaa56e1ebd04a2ad7884abfc
pratikdeoke <deokepratik3@gmail.com>
2026-05-28 23:51:05 +0530

    modify sendVerificationEmail.js

--- backend/src/utils/sendVerificationEmail.js
+++ backend/src/utils/sendVerificationEmail.js
@@ -1,36 +1,24 @@
+import "dotenv/config";
 import { Resend } from "resend";
 
-const resend = new Resend(
-  process.env.RESEND_API_KEY
-);
+console.log("FRONTEND_URL =", process.env.FRONTEND_URL);
 
-console.log(process.env.FRONTEND_URL);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendVerificationEmail =
-  async (email, token) => {
+export const sendVerificationEmail = async (email, token) => {
+  const verifyUrl =
+    `${process.env.FRONTEND_URL}/verify-email/${token}`;
 
-    const verifyUrl =
-      `${process.env.FRONTEND_URL}/verify-email/${token}`;
+  console.log("VERIFY URL =", verifyUrl);
 
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-
-      to: email,
-
-      subject: "Verify your email",
-
-      html: `
-        <h2>
-          Plant Disease Detection System
-        </h2>
-
-        <p>
-          Click below to verify your account:
-        </p>
-
-        <a href="${verifyUrl}">
-          Verify Email
-        </a>
-      `,
-    });
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Verify your email",
+    html: `
+      <h2>Plant Disease Detection System</h2>
+      <p>Click below to verify your account:</p>
+      <a href="${verifyUrl}">Verify Email</a>
+    `,
+  });
 };
