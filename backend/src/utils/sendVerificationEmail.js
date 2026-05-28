3970477cb1801e3c9f62c81b69ac5f5a193409c9
pratikdeoke <deokepratik3@gmail.com>
2026-05-28 23:20:46 +0530

    add sendVerificationEmail.js

---
+++ backend/src/utils/sendVerificationEmail.js
@@ -0,0 +1,34 @@
+import { Resend } from "resend";
+
+const resend = new Resend(
+  process.env.RESEND_API_KEY
+);
+
+export const sendVerificationEmail =
+  async (email, token) => {
+
+    const verifyUrl =
+      `${process.env.FRONTEND_URL}/verify-email/${token}`;
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+
+      to: email,
+
+      subject: "Verify your email",
+
+      html: `
+        <h2>
+          Plant Disease Detection System
+        </h2>
+
+        <p>
+          Click below to verify your account:
+        </p>
+
+        <a href="${verifyUrl}">
+          Verify Email
+        </a>
+      `,
+    });
+};
